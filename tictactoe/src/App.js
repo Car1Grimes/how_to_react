433add7da2d8c53cdc11b0aa0ad69e8870568ab0
thaivhm <hoangminhthaiv@gmail.com>
2025-03-30 22:54:19 +0700

    ...

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,26 +1,28 @@
 import { useState } from "react";
 
 function Square() {
-  return <button className="square"></button>;
+  const [value, setValue] = useState(null);
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
+  const [squares, getSquares] = useState(Array(9).fill(null));
   return (
     <>
-      <div className="square-row">
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
-      <div className="square-row">
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
-      <div className="square-row">
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
