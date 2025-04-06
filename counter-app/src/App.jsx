46fe1b9d3177e46a67f35f5daff351203a500d9b
thaivhm <hoangminhthaiv@gmail.com>
2025-04-07 00:05:18 +0700

    Handle increment/decrement functionalities

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,18 +1,47 @@
 import { useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleDecrease() {
+    if (counter === 0) {
+      return;
+    }
+    setCounter(counter - 1);
+  }
+
+  function handleIncrease() {
+    setCounter(counter + 1);
+  }
+
+  function handleReset() {
+    setCounter(0);
+  }
   return (
     <>
       <div className="content">
         <div className="container">
           <h1 className="title">Counter App</h1>
           <section className="counter-section">
-            <button className="btn">Decrease (-)</button>
+            <button
+              className="btn"
+              onClick={handleDecrease}
+              disabled={counter === 0}
+            >
+              Decrease (-)
+            </button>
             <div>
-              <span>0</span>
+              <span>{counter}</span>
             </div>
-            <button className="btn">Increase (+)</button>
+            <button className="btn" onClick={handleIncrease}>
+              Increase (+)
+            </button>
           </section>
+          <div>
+            <button className="btn--red" onClick={handleReset}>
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     </>
